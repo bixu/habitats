fa06a1b2b008ba5c376a9b56fb2aba38794a7767
Blake Irvin <blake.irvin@smartb.eu>
2016-06-15 20:38:24 +0200

    WIP: remove unneeded config and include bixu/ deps

--- memcached/plan.sh
+++ memcached/plan.sh
@@ -5,16 +5,10 @@ pkg_maintainer="Blake Irvin <blake.irvin@gmail.com>"
 pkg_license=('MIT')
 pkg_source=http://www.memcached.org/files/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=f058437b3c224d321919a9a6bb4e3eedb2312ed718c0caf087ff2f04ab795dda
-pkg_deps=(core/cacerts core/glibc core/git core/gcc)
-pkg_build_deps=()
+pkg_deps=(core/glibc)
+pkg_build_deps=(core/git core/gcc bixu/libevent core/make)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 pkg_svc_run="bin/memcached"
 pkg_expose=(11211)
-
-do_build() {
-  ./configure
-  make
-  make test
-}
